a402ba2faf2142c82ed0256e173d46886455f2b0
samau3 <samau415@gmail.com>
2024-12-17 20:14:05 -0800

    Add icon for giftbox

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,7 @@ import { defineQuery } from "next-sanity";
 
 import { client } from "@/sanity/client";
 import MaterialList from "./components/MaterialList";
+import { GiftIcon } from '@heroicons/react/24/solid'
 
 const options = { next: { revalidate: 60 } };
 
@@ -15,6 +16,7 @@ export default async function Home() {
     <div className="min-h-screen bg-gradient-to-br from-green-100 to-red-100 py-8">
       <div className="container mx-auto px-4">
         <h1 className="text-4xl font-bold mb-8 text-center text-red-800 shadow-text flex items-center justify-center">
+          <GiftIcon className="h-12 w-12 mr-4 text-green-700" />
           Santa&apos;s Toy Builder
         </h1>
         <MaterialList materials={materials} />
